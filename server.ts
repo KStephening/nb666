5044195b394d94223261572b00ae5bbbee7ba642
keeeeeeer <keeeeeeer@foxmail.com>
2023-03-05 20:20:49 +0800

    update

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { openai } from "https://deno.land/x/openai/mod.ts";
+import  { Configuration, OpenAIApi }  from "https://esm.sh//openai";
 const OPENAI_API_KEY = "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";
 
 async function handler(req: Request): Promise<Response> {
@@ -8,22 +8,32 @@ async function handler(req: Request): Promise<Response> {
     case "POST": {
       const { text } = await req.json().catch(() => null);
 
-      const model = "davinci";
-      const prompt = `The following is a conversation with an AI assistant. The assistant helps with various tasks.\n\nUser: ${text}\nAI:`;
+      // const model = "davinci";
+      // const prompt = `The following is a conversation with an AI assistant. The assistant helps with various tasks.\n\nUser: ${text}\nAI:`;
 
-      const result = await openai.createCompletion(
-        {
-          engine: model,
-          prompt,
-          maxTokens: 1024,
-          n: 1,
-          stop: "\n",
-          temperature: 0.5,
-        },
-        OPENAI_API_KEY
-      );
+      const configuration = new Configuration({
+        apiKey: OPENAI_API_KEY,
+      });
+      const openai = new OpenAIApi(configuration);
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        temperature: 0,
+        max_tokens: 7,
+      });
+      // const result = await openai.createCompletion(
+      //   {
+      //     engine: model,
+      //     prompt,
+      //     maxTokens: 1024,
+      //     n: 1,
+      //     stop: "\n",
+      //     temperature: 0.5,
+      //   },
+      //   OPENAI_API_KEY
+      // );
 
-      const response = result.choices[0].text.trim();
+      // const response = result.choices[0].text.trim();
       const body = { response };
       return new Response(body, {
         headers: { "content-type": "application/json" },
